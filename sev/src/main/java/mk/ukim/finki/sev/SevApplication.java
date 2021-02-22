7ac8865a2425608c1fc8d60b649b01b6b252148e
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2021-02-22 19:26:30 +0100

    Initial Spring Boot project

---
+++ sev/src/main/java/mk/ukim/finki/sev/SevApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.sev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevApplication.class, args);
+	}
+
+}
