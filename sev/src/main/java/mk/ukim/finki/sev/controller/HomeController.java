cce491464a10bd96939f314b119f9e053dba254a
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2021-03-10 21:27:20 +0100

    Updated Home Controller

--- sev/src/main/java/mk/ukim/finki/sev/controller/HomeController.java
+++ sev/src/main/java/mk/ukim/finki/sev/controller/HomeController.java
@@ -5,11 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String getHomePage() {
         return "index";
+    }
+    
+    @GetMapping("/about")
+    public String getAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String getContactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/voting")
+    public String getVotingPage() {
+        return "voting";
     }
 }
