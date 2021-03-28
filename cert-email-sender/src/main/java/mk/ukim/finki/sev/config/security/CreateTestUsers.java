97ff51f262028d37d4376c3f9b818d5e80de594d
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2021-03-28 23:50:13 +0200

    Voting page changes

---
+++ cert-email-sender/src/main/java/mk/ukim/finki/sev/config/security/CreateTestUsers.java
@@ -0,0 +1,19 @@
+package mk.ukim.finki.sev.config.security;
+
+import mk.ukim.finki.sev.model.Role;
+import mk.ukim.finki.sev.service.UserService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class CreateTestUsers {
+
+    private final UserService userService;
+
+    public CreateTestUsers(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
